f566d929f272f6c73f1fdbc2330e64e0acd423ee
Егор Б <69579563+egoor67@users.noreply.github.com>
2021-02-06 23:42:50 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,29 +1,38 @@
 import sys
 import sqlite3
-from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow, QTableWidget, QTableWidgetItem, \
-    QVBoxLayout
 from PyQt5 import uic
+from PyQt5.QtWidgets import QApplication, QMainWindow, QTableWidget, QTableWidgetItem
+
+EDIT_MODE = 1
+ADD_MODE = 0
 
 
-class MyWidget(QWidget):
+class MyWidget(QMainWindow):
     def __init__(self):
         super().__init__()
         self.init_ui()
+        self.setWindowTitle('Капучино')
+        self.setGeometry(300, 100, 800, 800)
 
-    def data(self):
+
+    def load_data(self):
         con = sqlite3.connect('coffee.splite.db')
         cur = con.cursor()
-        self.data = cur.execute("""
-        SELECT * FROM espresso
-        """).fetchall()
+        self.data = cur.execute("""SELECT * FROM espresso""").fetchall()
 
     def init_ui(self):
         uic.loadUi('main.ui', self)
-        self.v_layout = QVBoxLayout(self)
-        self.tableWidget = QTableWidget(self)
-        self.v_layout.addWidget(self.tableWidget)
-        self.data()
+        self.addBut.clicked.connect(self.addcoffe)
+        self.editBut.clicked.connect(self.editcoffe)
+        self.load_data()
         self.table()
+
+    def get_current_films_id(self):
+        row = int(self.tableWidget.currentRow())
+        if row == -1:
+            return -1
+        else:
+            return self.tableWidget.item(row, 0).text()
 
     def table(self):
         self.tableWidget.setColumnCount(7)
@@ -35,18 +44,89 @@ class MyWidget(QWidget):
             self.tableWidget.setRowCount(self.tableWidget.rowCount() + 1)
             for col in range(self.tableWidget.columnCount()):
                 self.tableWidget.setItem(row, col, QTableWidgetItem(str(self.data[row][col])))
+                
+    def addcoffe(self):
+        self.show_window_addEdit(ADD_MODE, 3)
+        
+    def editcoffe(self):
+        s = self.get_current_films_id()
+        if s != -1:
+            self.show_window_addEdit(EDIT_MODE, s)
 
-class Example(QMainWindow):
-    def __init__(self):
+    def show_window_addEdit(self, mode, index_id):
+        try:
+            self.w2 = MyWidget2(mode, index_id)
+        except BaseException as error:
+            print(error)
+        self.w2.show()
+
+
+class MyWidget2(QMainWindow):
+    def __init__(self, mode, index_id):
+        self.index_id, self.mode = int(index_id), int(mode)
+        self.button_text = ('Добавить', 'Изменить', 'Удалить')
+        self.header_text = ('Добавление кофе', 'Изменение кофе', 'Удаление кофе')
         super().__init__()
-        self.setWindowTitle('Эсспрессо')
-        self.setGeometry(300, 100, 800, 800)
-        widget = MyWidget()
-        self.setCentralWidget(widget)
+        uic.loadUi('addEditCoffeeForm.ui', self)
+        self.setWindowTitle(self.header_text[self.mode])
+        self.pushButton.setText(self.button_text[self.mode])
+        self.pushButton.clicked.connect(self.run)
+        if mode == EDIT_MODE:
+            self.LoadCurrentCoffe(index_id)
 
+    def LoadCurrentCoffe(self, index):
+        # print(index)
+        self.con = sqlite3.connect("coffee.splite.db")
+        cur = self.con.cursor()
+        index = int(index)
+        result = cur.execute(f"""SELECT DISTINCT * FROM espresso WHERE id = '{index}'""").fetchall()
+        self.con.close()
+        # print(result)
+        self.title.setText(str(result[0][1]))
+        self.roast_degree.setText(str(result[0][2]))
+        self.type.setText(str(result[0][3]))
+        self.taste.setText(str(result[0][4]))
+        self.price.setText(str(result[0][5]))
+        self.V.setText(str(result[0][6]))
+        
+    def run(self):
+        title = str(self.title.text())
+        roast_degree = str(self.roast_degree.text())
+        type_ = str(self.type.text())
+        taste = str(self.taste.text())
+        price = str(self.price.text())
+        v = str(self.V.text())
+        
+        self.con = sqlite3.connect("coffee.splite.db")
+        cur = self.con.cursor()
+        if self.mode == ADD_MODE:
+            result = cur.execute("""SELECT DISTINCT id FROM espresso ORDER BY id DESC""").fetchone()
+            new_id = int(result[0]) + 1
+            p = f"""INSERT INTO espresso (ID, title, roast_degree, type, taste, price, V) VALUES ({new_id},
+            '{title}', '{roast_degree}', '{type_}', '{taste}', '{price}', '{v}')"""
+
+        elif self.mode == EDIT_MODE:
+            p = f"""UPDATE espresso SET title='{title}', roast_degree='{roast_degree}',
+        type='{type_}', taste='{taste}', price='{price}', V='{v}' WHERE id = {self.index_id}"""
+
+        elif self.mode == DELETE_MODE:
+            p = f"""DELETE FROM films WHERE id = {self.index_id}"""
+
+        try:
+            cur.execute(p)
+        except sqlite3.Error as error:
+            print(error)
+        self.con.commit()
+        self.con.close()
+        ex.load_data()
+        ex.table()
+        self.close()
+        
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    ex = Example()
+    ex = MyWidget()
     ex.show()
     sys.exit(app.exec())
+
+
